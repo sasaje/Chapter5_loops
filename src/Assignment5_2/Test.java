1a5b766eb5123c9a2a58b80240e891293d81b8ab
sarasandager <sara590x@edu.easj.dk>
2020-10-01 11:48:14 +0200

    Assignment 5.2 test case made in comments only

--- src/Assignment5_2/Test.java
+++ src/Assignment5_2/Test.java
@@ -10,4 +10,31 @@ public class Test {
     public static void runTest(){
         System.out.println("Test initialized");
     }
+
+    public static void main(String[] args) {
+
+        // Test case 1 - correct answer in interval
+        // Input: Correct answer on the random generated multiplication questions.
+        // Forventet output: Correct
+
+        // Faktiske output: Correct
+
+        // Test case 2 - wrong answer in interval
+        // Input: incorrect answer on the random generated multiplication questions.
+        // Forventet output: Correct count is 0, 10x wrong on every questions
+
+        // Faktiske output: Correct count is 0, 10x wrong on every questions + it shows the right result
+
+        // Test case 3 - wrong answer outside interval
+        // Input: incorrect answer outside the interval on the random generated multiplication questions.
+        // Forventet output: Correct count is 0, 10x wrong on every questions
+
+        // Faktiske output: Correct count is 0, 10x wrong on every questions + it shows the right result
+
+        // Test case 4. - Error handling
+        // Input: wrong datatype e.g. a String
+        // Forventet output: Error
+
+        // Faktiske output: Error
+    }
 }
