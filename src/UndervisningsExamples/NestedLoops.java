aff80ae82d92596d9cd88dcff985928899c23c47
sarasandager <sara590x@edu.easj.dk>
2020-10-05 21:17:37 +0200

    Comments added

--- src/UndervisningsExamples/NestedLoops.java
+++ src/UndervisningsExamples/NestedLoops.java
@@ -13,14 +13,17 @@ public class NestedLoops {
     public static void main(String[] args) {
         System.out.println("        Multiplication table");
 
+        // display the number title
         System.out.print("   ");
         for (int j = 1; j <= 9; j++) {
             System.out.print("  " + j);
             System.out.println("----------------------------------");
 
+            // Display the table body
             for (int i = 1; i <= 9; i++) {
                 System.out.print(i + " | ");
 
+                // Display the product and align properly
                 for (int k = 1; k <= 9; k++) {
                     System.out.printf("%4d", i * j);
                 }
