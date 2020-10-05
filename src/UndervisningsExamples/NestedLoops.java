5d1132cd7e76753e084839683d33b1189206b25a
sarasandager <sara590x@edu.easj.dk>
2020-10-05 21:15:23 +0200

    Nested loops added

--- src/UndervisningsExamples/NestedLoops.java
+++ src/UndervisningsExamples/NestedLoops.java
@@ -8,5 +8,27 @@
 package UndervisningsExamples;
 
 public class NestedLoops {
+    // 5.7 Multiplication table
+
+    public static void main(String[] args) {
+        System.out.println("        Multiplication table");
+
+        System.out.print("   ");
+        for (int j = 1; j <= 9; j++) {
+            System.out.print("  " + j);
+            System.out.println("----------------------------------");
+
+            for (int i = 1; i <= 9; i++) {
+                System.out.print(i + " | ");
+
+                for (int k = 1; k <= 9; k++) {
+                    System.out.printf("%4d", i * j);
+                }
+                System.out.println("");
+
+            }
+
+        }
+    }
 
 }
